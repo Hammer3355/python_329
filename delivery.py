206af36f68dcf988a42adc54fed58f6fc731c639
Professional <ttkagentusolie@gmail.com>
2023-12-15 04:31:25 +0800

    Создал класс Product и инициализатор.

--- delivery.py
+++ delivery.py
@@ -9,3 +9,24 @@
 from dataclasses import dataclass
 import json
 from typing import List, Tuple, Union
+
+class Product:
+    """
+    dimensions => габариты,
+    weight => вес,
+    fragility => хрупкость: True/False,
+    price => хрупкость: цена,
+    category => категория,
+    name => название,
+    """
+    def __init__(self, dimentions: Tuple[float, float, float], weight: float, fragility: bool, price: float,
+                 category: str, name: str):
+        self.dimentions = dimentions
+        self.weiht = weight
+        self.fragility = fragility
+        self.price = price
+        self.category = category
+        self.name = name
+
+
+
