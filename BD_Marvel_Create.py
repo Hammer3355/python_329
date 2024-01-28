f62776b2a5ee744f022ccb570e0b6afd25146d1b
Professional <ttkagentusolie@gmail.com>
2024-01-29 06:53:23 +0800

    Добавлено поле identify

--- BD_Marvel_Create.py
+++ BD_Marvel_Create.py
@@ -19,21 +19,22 @@
 # Создаем новую таблицу MarvelCharacters_new с дополнительным полем уникального id записи
 # "id INTEGER PRIMARY KEY AUTOINCREMENT".
 
-# CREATE TABLE MarvelCharacters_new(
-#     id INTEGER PRIMARY KEY AUTOINCREMENT,
-#     page_id INTEGER,
-#     name TEXT,
-#     urlslug TEXT,
-#     ALIGN TEXT,
-#     EYE TEXT,
-#     HAIR TEXT,
-#     SEX TEXT,
-#     GSM TEXT,
-#     ALIVE TEXT,
-#     APPERANCES INTRGER,
-#     FIRST_APPERANCES TEXT,
-#     Year INTEGER
-# )
+# CREATE TABLE MarvelCharacters (
+#     page_id           INTEGER,
+#     name              TEXT,
+#     urlslug           TEXT,
+#     identify          TEXT,
+#     ALIGN             TEXT,
+#     EYE               TEXT,
+#     HAIR              TEXT,
+#     SEX               TEXT,
+#     GSM               TEXT,
+#     ALIVE             TEXT,
+#     APPEARANCES       INTEGER,
+#     FIRST_APPEARANCES TEXT,
+#     Year
+# );
+
 
 #
 
