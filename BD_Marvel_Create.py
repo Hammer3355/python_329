cabbd888fb1641e13f2278fb873e2e2399979c5c
Professional <ttkagentusolie@gmail.com>
2024-01-29 06:37:36 +0800

    Создаем новую таблицу MarvelCharacters_new

--- BD_Marvel_Create.py
+++ BD_Marvel_Create.py
@@ -16,5 +16,24 @@
 #     Year
 # )
 
+# Создаем новую таблицу MarvelCharacters_new с дополнительным полем уникального id записи
+# "id INTEGER PRIMARY KEY AUTOINCREMENT".
 
+# CREATE TABLE MarvelCharacters_new(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     page_id INTEGER,
+#     name TEXT,
+#     urlslug TEXT,
+#     ALIGN TEXT,
+#     EYE TEXT,
+#     HAIR TEXT,
+#     SEX TEXT,
+#     GSM TEXT,
+#     ALIVE TEXT,
+#     APPERANCES INTRGER,
+#     FIRST_APPERANCES TEXT,
+#     Year INTEGER
+# )
+
+#
 
