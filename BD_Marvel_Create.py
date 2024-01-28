2e75fd0ee41032ca1b0f083c59331434bf8c8ec8
Professional <ttkagentusolie@gmail.com>
2024-01-29 06:30:11 +0800

    Создаем таблицу MarvelCharacters

---
+++ BD_Marvel_Create.py
@@ -0,0 +1,20 @@
+# Создаем таблицу MarvelCharacters в базе данных marvel
+
+# CREATE TABLE MarvelCharacters(
+#     page_id INTEGER,
+#     name TEXT,
+#     urlslug TEXT,
+#     identify TEXT,
+#     ALIGN TEXT,
+#     EYE TEXT,
+#     HAIR TEXT,
+#     SEX TEXT,
+#     GSM TEXT,
+#     ALIVE TEXT,
+#     APPEARANCES INTEGER,
+#     FIRST_APPEARANCES TEXT,
+#     Year
+# )
+
+
+
